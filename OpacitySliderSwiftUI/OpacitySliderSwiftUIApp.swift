70d07686d7a7b7bd02f0f51f1f445227ecb498ad
MaratXcode <kosjachok8@yandex.ru>
2024-08-07 20:29:46 +0300

    Initial Commit

---
+++ OpacitySliderSwiftUI/OpacitySliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpacitySliderSwiftUIApp.swift
+//  OpacitySliderSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 07.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct OpacitySliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
