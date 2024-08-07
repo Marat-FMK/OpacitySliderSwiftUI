6c225df5aea323a8810265430e83209dbd4424d3
MaratXcode <kosjachok8@yandex.ru>
2024-08-07 21:24:54 +0300

    work with view

--- OpacitySliderSwiftUI/ContentView.swift
+++ OpacitySliderSwiftUI/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var targetValue: Int = Int.random(in: 1...100)
+    @State private var currentValue: Double = 50
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack (spacing: 29){
+            Text("Move the slide to the value: \(targetValue)")
+                .font(.system(size: 27))
+            HStack {
+                Text("0")
+                Spacer() // slider place
+                Text("100")
+            }
+            Button(action: checkResult) {
+                Text(" Check my result")
+                    .foregroundStyle(.mint)
+                    .font(.system(size: 27))
+            }
+            Button(action: restart) {
+                Text("Restart")
+                    .foregroundStyle(.red)
+                    .font(.system(size: 23))
+            }
         }
         .padding()
+    }
+    
+    private func computeScore() -> Int {
+        let differrence = abs(targetValue - lround(currentValue))
+        return 100 - differrence
+    }
+    
+    private func checkResult() {
+        
+    }
+    
+    private func restart() {
+        
     }
 }
 
---
+++ OpacitySliderSwiftUI/Slider.swift
@@ -0,0 +1,18 @@
+//
+//  Slider.swift
+//  OpacitySliderSwiftUI
+//
+//  Created by Marat Fakhrizhanov on 07.08.2024.
+//
+
+import SwiftUI
+
+struct Slider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Slider()
+}
